fe42ead86288169c007b34142d5d08878d74275d
Tui Tpk <tanamet.tpk@gmail.com>
2020-03-04 17:20:28 +0700

    Initial Commit

---
+++ SwiftUi-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi-practice
+//
+//  Created by Tanamet Tanasinpatcharakul on 4/3/2563 BE.
+//  Copyright © 2563 Tanamet Tanasinpatcharakul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
